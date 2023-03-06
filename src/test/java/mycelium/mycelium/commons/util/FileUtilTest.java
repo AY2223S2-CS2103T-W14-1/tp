96cebebfa1252e48bea443da7102d32d6b05c0db
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-03-06 16:32:55 +0800

    Rename packages

---
+++ src/test/java/mycelium/mycelium/commons/util/FileUtilTest.java
@@ -0,0 +1,25 @@
+package mycelium.mycelium.commons.util;
+
+import static mycelium.mycelium.testutil.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import mycelium.mycelium.commons.util.FileUtil;
+
+public class FileUtilTest {
+
+    @Test
+    public void isValidPath() {
+        // valid path
+        assertTrue(FileUtil.isValidPath("valid/file/path"));
+
+        // invalid path
+        assertFalse(FileUtil.isValidPath("a\0"));
+
+        // null path -> throws NullPointerException
+        assertThrows(NullPointerException.class, () -> FileUtil.isValidPath(null));
+    }
+
+}
---
+++ src/test/java/mycelium/mycelium/logic/commands/HelpCommandTest.java
@@ -0,0 +1,22 @@
+package mycelium.mycelium.logic.commands;
+
+import static mycelium.mycelium.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static mycelium.mycelium.logic.commands.HelpCommand.SHOWING_HELP_MESSAGE;
+
+import org.junit.jupiter.api.Test;
+
+import mycelium.mycelium.logic.commands.CommandResult;
+import mycelium.mycelium.logic.commands.HelpCommand;
+import mycelium.mycelium.model.Model;
+import mycelium.mycelium.model.ModelManager;
+
+public class HelpCommandTest {
+    private Model model = new ModelManager();
+    private Model expectedModel = new ModelManager();
+
+    @Test
+    public void execute_help_success() {
+        CommandResult expectedCommandResult = new CommandResult(SHOWING_HELP_MESSAGE, true, false);
+        assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
+    }
+}
