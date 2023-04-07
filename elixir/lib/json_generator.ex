59fcb47d1edd9753f62ae53950bd612f07ccf82e
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-04-07 11:31:14 +0800

    Remove old field from random data

--- elixir/lib/json_generator.ex
+++ elixir/lib/json_generator.ex
@@ -9,7 +9,6 @@ defmodule JsonGenerator do
         name: Faker.String.naughty() <> "foo", # to ensure non empty
         email: Enum.fetch!(emails, i - 1),
         source: Faker.String.naughty() <> "foo",
-        entityName: "Client",
         yearOfBirth: to_string(:rand.uniform(9999)) |> String.pad_leading(4, "0"),
         mobileNumber: Faker.Phone.PtPt.cell_number()
       }
@@ -30,8 +29,7 @@ defmodule JsonGenerator do
         source: Faker.String.naughty() <> "foo",
         description: Faker.String.naughty(),
         acceptedOn: Calendar.strftime(Faker.Date.between(~D[2010-12-10], ~D[2016-12-25]), "%d/%m/%Y"),
-        deadline: Calendar.strftime(Faker.Date.between(~D[2010-12-10], ~D[2016-12-25]), "%d/%m/%Y"),
-        entityName: "Project"
+        deadline: Calendar.strftime(Faker.Date.between(~D[2010-12-10], ~D[2016-12-25]), "%d/%m/%Y")
       }
     end)
   end
